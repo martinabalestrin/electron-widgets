f95e6ffe46473bf3b9758f836ccf159569fa3eaf
Martina Balestrin <balestrinmartina@gmail.com>
2026-01-11 17:36:49 -0300

    Basic Template for Electron Widgets!

---
+++ basic-template/main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 292,
+        height: 430,
+        resizable: false,
+        maximizable: false,
+        fullscreenable: false,
+        frame: false,
+        transparent: false,
+        webPreferences: {
+            contextIsolation: true
+        }
+    });
+
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
